14afc040e4a92ed2430ccce969aa94d221f79eff
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-03-24 17:03:09 +0300

    added changes

---
+++ arrays.js
@@ -0,0 +1,21 @@
+
+ let arr1=[3,7,34,90,12];
+ console.log('last item',arr1.slice(-1))
+
+
+ let arr2=[true,"green","where",12,56];
+ console.log('last item',arr2.slice(-1));
+
+
+
+ let myPets=["Cow","Bird","Snake","Dog"];
+console.log(myPets.join(""))
+
+
+let arr3=[-5,9,3,2,-3,6,8,4,1];
+console.log((arr3.sort()))
+
+
+
+ let word="renniw";
+console.log(word.split('').sort().join(''));
